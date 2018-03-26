0b6e88370353e8c00ec3aaef70ed593a8dccde78
James Allenspach <james.allenspach@cision.com>
2018-03-26 16:08:13 -0500

    Rearranged graphs

--- website/graphs.js
+++ website/graphs.js
@@ -123,6 +123,72 @@ var GRAPH = [
 		}
 	},
 	{
+		"title": "Group by decade",
+		"id": "decade-count",
+		"graphType": "movies-list",
+		"prepFn": function() {
+			var sparseColorArray = Array();
+			var years = new Array();
+			function initColorArray() {
+				for (var i = 1950; i < 2020; i += 10) {
+					var baseColor;
+					switch (i) {
+						case 1950: baseColor = Array(255,204,153); break;
+						case 1960: baseColor = Array(224,224,224); break;
+						case 1970: baseColor = Array(255,153,153); break;
+						case 1980: baseColor = Array(153,204,255); break;
+						case 1990: baseColor = Array(255,255,153); break;
+						case 2000: baseColor = Array(153,255,255); break;
+						case 2010: baseColor = Array(204,153,255); break;
+					}
+					sparseColorArray[i] = '#' + ('0'+baseColor[0].toString(16)).substr(-2) + ('0'+baseColor[1].toString(16)).substr(-2) + ('0'+baseColor[2].toString(16)).substr(-2);
+				}
+			}
+			initColorArray();
+			for (var m = 1; m < SHOWS.length; ++m)
+				if (SHOWS[m].movie != null && MOVIES[SHOWS[m].movie] && /^\d\d\d\d/.test(MOVIES[SHOWS[m].movie].release_date)) {
+					var y = /^(\d\d\d)/.exec(MOVIES[SHOWS[m].movie].release_date)[1];
+					if (years[y * 10])
+						++years[y * 10];
+					else 
+						years[y * 10] = 1;
+				}
+			var labelData = Array(),barLabelData = Array(),valueData = Array(), bkgColor = Array();
+			for (var iter = 1900; iter < years.length; iter += 10) {
+				if (years[iter] > 0) {
+					labelData.push(iter+'s');
+					barLabelData.push('Number of movies in the '+iter+'s');
+					valueData.push(years[iter]);
+					bkgColor.push(sparseColorArray[iter]);
+				}
+
+			}
+			return {
+				type: 'bar',
+				data: { 
+					datasets: [{
+						label: 'Movies grouped by decade',
+						"data": valueData,
+						backgroundColor: bkgColor,
+						datalabels: {
+							display: false
+						}
+					}],
+					labels: labelData
+				},
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			}; 
+		}
+	},
+	{
 		"title": "Group by year",
 		"id": "year-count",
 		"graphType": "movies-list",
@@ -200,72 +266,6 @@ var GRAPH = [
 							}
 						}
 					},
-					scales: {
-						yAxes: [{
-							ticks: {
-								beginAtZero: true
-							}
-						}]
-					}
-				}
-			}; 
-		}
-	},
-	{
-		"title": "Group by decade",
-		"id": "decade-count",
-		"graphType": "movies-list",
-		"prepFn": function() {
-			var sparseColorArray = Array();
-			var years = new Array();
-			function initColorArray() {
-				for (var i = 1950; i < 2020; i += 10) {
-					var baseColor;
-					switch (i) {
-						case 1950: baseColor = Array(255,204,153); break;
-						case 1960: baseColor = Array(224,224,224); break;
-						case 1970: baseColor = Array(255,153,153); break;
-						case 1980: baseColor = Array(153,204,255); break;
-						case 1990: baseColor = Array(255,255,153); break;
-						case 2000: baseColor = Array(153,255,255); break;
-						case 2010: baseColor = Array(204,153,255); break;
-					}
-					sparseColorArray[i] = '#' + ('0'+baseColor[0].toString(16)).substr(-2) + ('0'+baseColor[1].toString(16)).substr(-2) + ('0'+baseColor[2].toString(16)).substr(-2);
-				}
-			}
-			initColorArray();
-			for (var m = 1; m < SHOWS.length; ++m)
-				if (SHOWS[m].movie != null && MOVIES[SHOWS[m].movie] && /^\d\d\d\d/.test(MOVIES[SHOWS[m].movie].release_date)) {
-					var y = /^(\d\d\d)/.exec(MOVIES[SHOWS[m].movie].release_date)[1];
-					if (years[y * 10])
-						++years[y * 10];
-					else 
-						years[y * 10] = 1;
-				}
-			var labelData = Array(),barLabelData = Array(),valueData = Array(), bkgColor = Array();
-			for (var iter = 1900; iter < years.length; iter += 10) {
-				if (years[iter] > 0) {
-					labelData.push(iter+'s');
-					barLabelData.push('Number of movies in the '+iter+'s');
-					valueData.push(years[iter]);
-					bkgColor.push(sparseColorArray[iter]);
-				}
-
-			}
-			return {
-				type: 'bar',
-				data: { 
-					datasets: [{
-						label: 'Movies grouped by decade',
-						"data": valueData,
-						backgroundColor: bkgColor,
-						datalabels: {
-							display: false
-						}
-					}],
-					labels: labelData
-				},
-				options: {
 					scales: {
 						yAxes: [{
 							ticks: {
@@ -458,67 +458,6 @@ var GRAPH = [
 		}
 	},
 	{
-		"title": "Most profitable movies",
-		"id": "movie-profit",
-		"graphType": "movies-list",
-		"prepFn": function() {
-			var sortArray = Array();
-			for (var iter = 1; iter < SHOWS.length; ++iter) {
-				if (! SHOWS[iter]) continue;
-				if (! SHOWS[iter].movie) continue;
-				if (MOVIES[SHOWS[iter].movie].budget > 0 && MOVIES[SHOWS[iter].movie].revenue > 0)
-					sortArray.push({ id: SHOWS[iter].movie, 
-						budget: MOVIES[SHOWS[iter].movie].budget,
-						revenue: MOVIES[SHOWS[iter].movie].revenue,
-						profit: MOVIES[SHOWS[iter].movie].revenue / MOVIES[SHOWS[iter].movie].budget });
-			}
-			sortArray.sort(function(a,b) { return b.profit - a.profit });
-			var labelArray = Array(), movieIdArray = Array(), valueArray = Array(), tooltipArray = Array();
-			for (var i = 0; i < 25; ++i) {
-				labelArray[i] = MOVIES[sortArray[i].id].title;
-				valueArray[i] = sortArray[i].profit * 100;
-				movieIdArray[i] = sortArray[i].id;
-				tooltipArray[i] = Array("Budget: $" + sortArray[i].budget.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"),
-					"Revenue: $"+sortArray[i].revenue.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"),
-					"Profit: " + String(Math.floor(sortArray[i].revenue / sortArray[i].budget * 10000) / 100) + '%'
-					);
-			}
-			return {
-				type: 'horizontalBar',
-				data: {
-					datasets: [{
-						label: "The most profitable movies",
-						data: valueArray,
-						backgroundColor: [ '#00ff80','#33cc80','#669980','#996680','#993300','#ff0000','#cc0033','#990066','#660099','#3300cc','#0000ff','#0033cc','#006699','#009966','#00cc33','#00ff00','#00ff33','#00ff66','#00ff99','#00ffcc','#00ffff','#00cccc','#339999','#665599','#990099' ],
-						datalabels: {
-							display: false
-						}
-					}],
-					labels: labelArray
-				},
-				options: {
-					tooltips: {
-						position: 'atMouse',
-						callbacks: {
-							label: function(tooltipItem, data) {
-								var num = tooltipItem.index;
-								return tooltipArray[num];
-							}
-						}
-					},
-					scales: {
-						xAxes: [{
-							ticks: {
-								beginAtZero: true
-							}
-						}]
-					}
-				}
-			};
-		}
-	},
-
-	{
 		"title": "Least profitable movies",
 		"id": "movie-least-profit",
 		"graphType": "movies-list",
@@ -577,5 +516,65 @@ var GRAPH = [
 				}
 			};
 		}
-	}
+	},
+	{
+		"title": "Most profitable movies",
+		"id": "movie-profit",
+		"graphType": "movies-list",
+		"prepFn": function() {
+			var sortArray = Array();
+			for (var iter = 1; iter < SHOWS.length; ++iter) {
+				if (! SHOWS[iter]) continue;
+				if (! SHOWS[iter].movie) continue;
+				if (MOVIES[SHOWS[iter].movie].budget > 0 && MOVIES[SHOWS[iter].movie].revenue > 0)
+					sortArray.push({ id: SHOWS[iter].movie, 
+						budget: MOVIES[SHOWS[iter].movie].budget,
+						revenue: MOVIES[SHOWS[iter].movie].revenue,
+						profit: MOVIES[SHOWS[iter].movie].revenue / MOVIES[SHOWS[iter].movie].budget });
+			}
+			sortArray.sort(function(a,b) { return b.profit - a.profit });
+			var labelArray = Array(), movieIdArray = Array(), valueArray = Array(), tooltipArray = Array();
+			for (var i = 0; i < 25; ++i) {
+				labelArray[i] = MOVIES[sortArray[i].id].title;
+				valueArray[i] = sortArray[i].profit * 100;
+				movieIdArray[i] = sortArray[i].id;
+				tooltipArray[i] = Array("Budget: $" + sortArray[i].budget.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"),
+					"Revenue: $"+sortArray[i].revenue.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"),
+					"Profit: " + String(Math.floor(sortArray[i].revenue / sortArray[i].budget * 10000) / 100) + '%'
+					);
+			}
+			return {
+				type: 'horizontalBar',
+				data: {
+					datasets: [{
+						label: "The most profitable movies",
+						data: valueArray,
+						backgroundColor: [ '#00ff80','#33cc80','#669980','#996680','#993300','#ff0000','#cc0033','#990066','#660099','#3300cc','#0000ff','#0033cc','#006699','#009966','#00cc33','#00ff00','#00ff33','#00ff66','#00ff99','#00ffcc','#00ffff','#00cccc','#339999','#665599','#990099' ],
+						datalabels: {
+							display: false
+						}
+					}],
+					labels: labelArray
+				},
+				options: {
+					tooltips: {
+						position: 'atMouse',
+						callbacks: {
+							label: function(tooltipItem, data) {
+								var num = tooltipItem.index;
+								return tooltipArray[num];
+							}
+						}
+					},
+					scales: {
+						xAxes: [{
+							ticks: {
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			};
+		}
+	}	
 ];
