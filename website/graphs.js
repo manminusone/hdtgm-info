4843b9b12e7f6ca718554ef2fb3594d6ebc58cd4
James Allenspach <jima@legnog.com>
2018-12-07 06:13:03 -0600

    Updated some graph axis labels to more readable versions

--- website/graphs.js
+++ website/graphs.js
@@ -474,7 +474,13 @@ var GRAPH = [
 					scales: {
 						xAxes: [{
 							ticks: {
-								beginAtZero: true
+								beginAtZero: true,
+								callback: function(value, index, values) {
+									if (value >= 1000000)
+										return '$' + Math.round(value / 1000000) + 'M';
+									else
+										return '$' + value;
+								}
 							}
 						}]
 					}
@@ -537,7 +543,10 @@ var GRAPH = [
 					scales: {
 						xAxes: [{
 							ticks: {
-								beginAtZero: true
+								beginAtZero: true,
+								callback: function(value, index, values) {
+									return value + '%';
+								}
 							}
 						}]
 					}
@@ -600,7 +609,10 @@ var GRAPH = [
 					scales: {
 						xAxes: [{
 							ticks: {
-								beginAtZero: true
+								beginAtZero: false,
+								callback: function(value, index, values) {
+									return value + '%';
+								}
 							}
 						}]
 					}
